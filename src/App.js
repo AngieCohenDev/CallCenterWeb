93e8aaa7b8c7f9a55fe8c136c41ba4d050c60522
Angie Cohen <angiedev2001@gmail.com>
2023-05-16 12:06:21 -0300

    Creacion de las reglas basicas de css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+
 import { Blog, Header, Footer, Features, Possibility, Tesbacon } from './containers';
 import {CTA, Brand, Navbar} from './components';
+import './App.css';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+      </div>
+      <Brand />
+      <Tesbacon/>
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
 
