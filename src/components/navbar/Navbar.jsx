fac67da5d94aaaec4bd4a9b7abcf17289ff181c0
Angie Cohen <angiedev2001@gmail.com>
2023-05-16 14:00:12 -0300

    Creado y estructurado el componente Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
-import './navbar.css'
+import React, {useState} from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.png";
+import "./navbar.css";
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#aboutUs">Sobre Nosotros</a>
+    </p>
+    <p>
+      <a href="#servicios">Servicios</a>
+    </p>
+    <p>
+      <a href="#cursos">Cursos</a>
+    </p>
+    <p>
+      <a href="#experiencia">Experiencia</a>
+    </p>
+    <p>
+      <a href="#contacto">Contacto</a>
+    </p>
+  </>
+);
+
 
 const Navbar = () => {
+  
+  const [toggleMenu, setToggleMenu] = useState(false)
+  
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      <div className="navbar-links">
+        
+        <div className="navbar-links_container">
+          <Menu />
+        </div>
+        <div className="navbar-menu">
+        {toggleMenu ? 
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+         : 
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        }
+        {toggleMenu && (
+          <div className="navbar-menu_container scale-up-center">
+            <div className="navbar-menu_container-links">
+              <Menu />
+            </div>
+          </div>
+        )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
