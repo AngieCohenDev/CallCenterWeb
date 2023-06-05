67a4edd0eca24e4c45513d4391bb9242ac9efc40
Angie Cohen <angiedev2001@gmail.com>
2023-06-05 11:37:50 -0300

    se agrega la parte responsive del componante header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -10,20 +10,22 @@ const Header = () => {
     <div className="header section__padding" id="home">
       <div className="header-content">
         <h1>Somos Tesbacon</h1>
-        <h2>Somos la herramienta que tu empresa necesita</h2>
+        <h2 className="subtitle"> La herramienta que tu empresa necesita</h2>
         <p>
           Somos la solución que su empresa necesita. Estamos encantados de
           formar parte de su equipo, contáctanos vía WhatsApp, LinkedIn o correo
           electrónico para agendar una entrevista personal.
         </p>
-        
+      
+        <div className="header-down">
+          <button type="button">CONTÁCTANOS</button>
           <div className="header-social_links">
             <img src={whatsapp} alt="whatsapp" />
             <img src={gmail} alt="gmail" />
             <img src={linkedin} alt="linkedin" />
             <img src={instagran} alt="instagram" />
           </div>
-       
+        </div>
       </div>
     </div>
   );
