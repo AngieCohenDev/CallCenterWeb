8d2ded2c483bc6e4ed3e24365d68ccf2b82def68
Angie Cohen <angiedev2001@gmail.com>
2023-06-08 14:33:09 -0300

    estructura del componente blog

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
       <Tesbacon/>
       <CTA />
       <Blog />
+      <CTA />
       <Features />
       <Possibility />
       <Footer />
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import './blog.css'
+import React from "react";
+import "./blog.css";
+import services from "../../assets/Services.png";
+import Why_Us from "../../assets/Why_Us.png";
+import Why_Us1 from "../../assets/Why_Us1.png";
+
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="blog section__padding" id="unete">
+      <div className="blog-content">
+        <h1>Se parte del equipo Tesbacon </h1>
+        <p>
+          Constantemente estamos en busca de talentos que quieran expandir sus
+          habilidades en el mercado laboral. En nuestra empresa vas a laborar
+          con un maravilloso equipo de profesionales, contamos con horarios
+          flexibles y una gran remuneración económica.{" "}
+        </p>
+        <div>
+          <h2>
+            ¡No esperes más! Obtén la oportunidad de trabajar con una empresa de
+            primer nivel.
+          </h2>
+        </div>
+        <div>
+          <p>
+            Envíanos tu curriculum o ingresa a nuestro LinkedIn y postules en la
+            vacante que más se ajuste a tu perfil
+          </p>
+        </div>
+        <div className="blog_content-red">
+          <img src={Why_Us} alt={Why_Us} />
+          <img src={Why_Us1} alt={Why_Us1} />
+        </div>
+      </div>
+      <div className="blog-image">
+        <img src={services} alt={services} />
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
