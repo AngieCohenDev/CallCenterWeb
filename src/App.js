2340b3d79b0c98dd11649f0df879ce978156ccd4
Angie Cohen <angiedev2001@gmail.com>
2023-06-08 14:57:58 -0300

    se empezo a construir el footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react'
+import React from "react";
 
-import { Blog, Header, Footer, Features, Possibility, Tesbacon } from './containers';
-import {CTA, Brand, Navbar} from './components';
-import './App.css';
+import {
+  Blog,
+  Header,
+  Footer,
+  Features,
+  Possibility,
+  Tesbacon,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='App'>
-      <div className='back'>
-          <Navbar />
-          <Header />
+    <div className="App">
+      <div className="back">
+        <Navbar />
+        <Header />
       </div>
-      <Tesbacon/>
+      <Tesbacon />
       <CTA />
       <Blog />
       <CTA />
       <Features />
       <Possibility />
-      <Footer />
+      <div className="style">
+        <Footer/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './footer.css'
+import logo from '../../assets/logo.png'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer-content'>
+        <div className='footer-content_image'>
+          <img src={logo} alt={logo} />
+        </div>
+        <div className='footer_title section__padding'>
+          <h1>Encuéntranos aquí</h1>
+        </div>
+      </div>
+     
+    </div>
   )
 }
 
